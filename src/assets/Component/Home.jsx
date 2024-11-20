0a6449f0593983db53fee8c78ebc4690fee83d68
tushar <tushar@gmail.com>
2024-11-20 14:35:31 +0530

    'responsive'

--- src/assets/Component/Home.jsx
+++ src/assets/Component/Home.jsx
@@ -22,47 +22,49 @@ export default function Home() {
           setWeather(oldweatherdata);
         })
         .catch((error) => {
-          toast.error("city not found");
+          toast.error("City not found");
         });
     } else {
-      toast.error("allready exits");
+      toast.error("Already exists");
     }
   };
+
   useEffect(() => {
     console.log(weather);
   }, [weather]);
 
   return (
     <>
-      <div className="max-w-[100%] xs:max-w-[100%] ">
-        <div className="max-w-[1320px] mx-auto ">
-          <h1 class="text-5xl p-4   font-bold from-gray-700 via-red-600 to-green-600 bg-gradient-to-bl bg-clip-text text-transparent">
-            search city name
+      <div className="max-w-full xs:max-w-full">
+        <div className="max-w-6xl mx-auto">
+          <h1 className="xl:text-5xl  p-4 font-bold text-transparent bg-gradient-to-bl from-gray-700 via-red-600 to-green-600 bg-clip-text">
+            Search City Name
           </h1>
         </div>
-        <div className="max-w-[1320px] xs-w-[1320px] mx-auto mt-20">
+        <div className="max-w-6xl mx-auto xl:mt-20">
           <ToastContainer />
-          <form className="max-w-[500px]  rounded" onSubmit={formhandler}>
+          <form className="max-w-md mx-auto rounded flex" onSubmit={formhandler}>
             <input
               type="text"
               value={cityname}
               onChange={(event) => setCityName(event.target.value)}
-              className="w-[80%] rounded-xl p-2  outline-none"
+              className="w-full rounded-xl p-2 outline-none"
+              placeholder="Enter city name"
             />
-            <button className="w-[20%] p-2 bg-gradient-to-r from-teal-400 to-blue-500 hover:from-pink-500 hover:to-orange-500 rounded-xl">
-              save me
+            <button className="w-1/3 p-2 bg-gradient-to-r from-teal-400 to-blue-500 hover:from-pink-500 hover:to-orange-500 rounded-xl">
+              Save Me
             </button>
           </form>
         </div>
-        <div className="grid grid-cols-4  mt-4 max-w-[1320px] mx-auto">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-4 max-w-6xl mx-auto">
           {weather.length >= 1
             ? weather.map((value, index) => {
                 return (
-                  <div className="border-2  w-[300px] mt-2  text-2xl  p-5  rounded-2xl font-bold font-serif bg-gradient-to-r from-blue-200 from-1% via-sky-100 via-25% to-yellow-200 to-90%  hover:to-red-300 ">
-                    <span className="">
+                  <div className="border-2 w-full mt-2 text-2xl p-5 rounded-2xl font-bold font-serif bg-gradient-to-r from-blue-200 via-sky-100 to-yellow-200 hover:to-red-300" key={index}>
+                    <span>
                       {value.name} &nbsp; <mark>{value.sys.country}</mark>
                     </span>
-                    <p className=" text-2xl">{value.main.temp}</p>
+                    <p className="text-2xl">{value.main.temp} °C</p>
                     <img
                       src={`https://openweathermap.org/img/w/${value.weather[0].icon}.png`}
                       alt=""
